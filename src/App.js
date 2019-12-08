aa951c595e559a1f0012401669b9349909d748f9
Kyle Wilichowski <kdwilich@uark.edu>
2019-12-08 14:54:03 -0600

    created cart page functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/Nav";
-import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 import cart_img from "./assets/images/cart.png";
-import Image from "react-bootstrap/Image";
 
 class App extends Component {
   state = {
@@ -16,13 +11,11 @@ class App extends Component {
     book: {
       name: "asdf",
       ISBN: 12341523477
-    },
-    cart: []
+    }
   };
 
   componentDidMount() {
     this.getBooks();
-    this.getCart();
   }
 
   getBooks = _ => {
@@ -32,71 +25,56 @@ class App extends Component {
       .catch(err => console.error(err));
   };
 
-  getCart = _ => {
-    fetch("https://bookstore-server-t12.herokuapp.com/carts")
-      .then(response => response.json())
-      .then(response => this.setState({ cart: response.data }))
-      .catch(err => console.error(err));
-  };
-
   addBookToCart = _ => {
     const { book } = this.state;
     fetch(
       `https://bookstore-server-t12.herokuapp.com/carts/add?AccountID=170693&cartID=1&Book=${book.name}&ISBN=${book.ISBN}&Cart_Quantity=1`
     )
       .then(console.log(book))
-      .then(this.getCart)
       .catch(err => console.error(err));
   };
 
-  renderBooks = ({ ISBN, title, Authors, Publisher, price, edition }) => (
-    <Card className="m-2" style={{ width: "18rem" }}>
-      <Card.Body>
-        <Card.Title>
-          {title}, {edition} Edition
-        </Card.Title>
-        <Card.Text>
-          <b>Author: </b>
-          {Authors} <br />
-          <b>Publisher: </b>
-          {Publisher} <br />
-          <b>ISBN: </b>
-          {ISBN} <br />
-        </Card.Text>
-      </Card.Body>
-      <Card.Footer>
-        <div className="d-flex justify-content-between">
-          <span className="px-2 d-flex align-items-center">
-            <b>${price}</b>
-          </span>
-          <Button
-            key={ISBN}
-            onClick={this.setState(
-              { book: { name: { title }, ISBN: { ISBN } } },
-              () => this.addBookToCart
-            )}
-            variant="info"
-          >
-            Add to cart
-          </Button>
-        </div>
-      </Card.Footer>
-    </Card>
-  );
+  onClickFn(idk) {
+    console.log(idk);
+  }
 
-  renderCart = ({ Book, ISBN }) => (
-    <div>
-      <div>Book: {Book}</div>
-      <div>ISBN: {ISBN}</div>
-      <br />
+  renderBooks = ({ ISBN, title, Authors, Publisher, price, edition }) => (
+    <div key={ISBN}>
+      <Card className="m-2" style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>
+            {title}, {edition} Edition
+        </Card.Title>
+          <Card.Text>
+            <b>Author: </b>
+            {Authors} <br />
+            <b>Publisher: </b>
+            {Publisher} <br />
+            <b>ISBN: </b>
+            {ISBN} <br />
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <div className="d-flex justify-content-between">
+            <span className="px-2 d-flex align-items-center">
+              <b>${price}</b>
+            </span>
+            <Button
+              onClick={this.onClickFn(this.state.book)}
+              variant="info"
+            >
+              Add to cart
+          </Button>
+          </div>
+        </Card.Footer>
+      </Card>
     </div>
   );
 
   render() {
-    const { books, cart } = this.state;
+    const { books } = this.state;
     return (
       <div className="p-3 d-flex flex-column">
-        {/* <Button variant="info">Info</Button> */}
         <div className="d-flex justify-content-between">
           <div className="header">Bookstore</div>
           <a href="cart" style={{ textDecoration: "none", fontSize: "25px" }}>
@@ -104,7 +82,7 @@ class App extends Component {
             <img width="25px" height="25px" src={cart_img} />
           </a>
         </div>
-        {/* <div className="d-flex flex-wrap">{cart.map(this.renderCart)}</div> */}
+        {/* <div className="d-flex flex-wrap">{books.map(this.renderBooks)}</div> */}
         <div className="d-flex flex-wrap">{books.map(this.renderBooks)}</div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 import "./index.css";
 import App from "./App";
 import Counters from "./components/counters";
+import Cart from "./components/cart";
+import NotFound from "./components/notfound";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/cart" component={Cart} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+)
+
+
+ReactDOM.render(routing, document.getElementById("root"));
 serviceWorker.unregister();
