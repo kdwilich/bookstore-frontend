c72d264408bf3a86fcdc2a3fdd51cafee2f8638e
Kyle Wilichowski <kdwilich@uark.edu>
2019-12-07 22:51:08 -0600

    updated interface

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import cart from './assets/images/cart.png'
 
 class App extends Component {
   state = {
@@ -17,13 +25,36 @@ class App extends Component {
       .catch(err => console.error(err));
   };
 
-  renderBooks = ({ ISBN, title }) => <div key={ISBN}>{title}</div>;
+  renderBooks = ({ ISBN, title, Authors, Publisher, price, Stock, year, category, edition }) => (
+    <Card className="m-2" style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{title}, {edition} Edition</Card.Title>
+        <Card.Text>
+          <b>Author: </b>{Authors} <br />
+          <b>Publisher: </b>{Publisher} <br />
+          <b>ISBN: </b>{ISBN} <br />
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <div className="d-flex justify-content-between">
+          <span className="px-2 d-flex align-items-center"><b>${price}</b></span>
+          <Button variant="info">Add to cart</Button>
+        </div></Card.Footer>
+    </Card>
+  );
 
   render() {
     const { books } = this.state;
     return (
-      <div className="App App-header">
-        <div>{books.map(this.renderBooks)}</div>
+      <div className="App p-3 d-flex flex-column">
+        {/* <Button variant="info">Info</Button> */}
+        <div className="d-flex justify-content-between">
+          <div className="header">Bookstore</div>
+          <a href="cart" style={{ fontSize: '25px' }}>Cart
+            <img width="25px" height="25px" src={cart} />
+          </a>
+        </div>
+        <div className="d-flex flex-wrap">{books.map(this.renderBooks)}</div>
       </div>
     );
   }
